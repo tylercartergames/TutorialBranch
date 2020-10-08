974909acd3c8e964a0b43ff849ed55d16493958c
episodicjourney <episodicjourney@gmail.com>
2020-10-08 13:06:01 -0600

    did stuff
    These are changes i made to test if it worked. Going to do a pull request now.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //OH JESUS THE DELAY IS REAL OMG THIS IS DEALYED.
+
+    //HERE IS ANOTHER COMMENT
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
